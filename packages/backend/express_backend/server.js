2471e438136752fd50ab14e55fa2874ba2c0b1c1
kimulu <kimulumichael@gmail.com>
2025-11-01 12:12:49 +0300

    change file limit size on server.js

--- packages/backend/express_backend/server.js
+++ packages/backend/express_backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json()); // To parse JSON request bodies
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors()); // Allow cross-origin requests
 
 // Connect to MongoDB
